dca36e74cba02af3db164927db20b4b451685ec9
hrqmartins <hrqmartins129@gmail.com>
2026-06-08 22:48:27 -0300

    feat: Adicionando navegação  Patientes

--- void/src/routes/AppNavigator.tsx
+++ void/src/routes/AppNavigator.tsx
@@ -8,6 +8,7 @@ import { DashboardScreen } from '../screens/DashboardScreen';
 import { TelemetryScreen } from '../screens/TelemetryScreen';
 import { PatientsScreen } from '../screens/PatientsScreen';
 import { ProfileScreen } from '../screens/ProfileScreen';
+import { AddPatientScreen } from '../screens/AddPatientScreen';
 
 export type RootStackParamList = {
   Welcome: undefined;
@@ -16,6 +17,7 @@ export type RootStackParamList = {
   Telemetry: { patientId: number };
   Patients: undefined;
   Profile: undefined;
+  AddPatient: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -23,21 +25,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Welcome"
-        screenOptions={{ headerShown: false, animation: 'fade' }}
-      >
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false, animation: 'fade' }}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
-
         <Stack.Screen name="Login" component={LoginScreen} />
-
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
-
         <Stack.Screen name="Telemetry" component={TelemetryScreen} />
-
         <Stack.Screen name="Patients" component={PatientsScreen} />
-
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="AddPatient" component={AddPatientScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
